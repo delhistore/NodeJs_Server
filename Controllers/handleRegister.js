1863b1b97849c33a9804ac96d646216f894e7ad0
tjshanu <tathagatdtu@gmail.com>
2018-08-24 12:45:09 +0530

    Update handleRegister.js

--- Controllers/handleRegister.js
+++ Controllers/handleRegister.js
@@ -3,7 +3,7 @@ handleRegister = (req,res,db,bcrypt) => {
 	const {name,email,password} = req.body;
 
 	if(!name || !email || !password) {
-		return res.status(400).json('incorrect form submission'));
+		return res.status(400).json('incorrect form submission');
 	}
 
 	const hash = bcrypt.hashSync(password);
